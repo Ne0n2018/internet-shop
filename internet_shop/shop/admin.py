dddc88e5def4696c1c6765a158f3e3c9434a993f
Ne0n2018 <beevertm@gmail.com>
2024-01-23 22:02:33 +0300

    view Product was added. Tamplate Product was created.

--- internet_shop/shop/admin.py
+++ internet_shop/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Cotegory
+from .models import Product, Category
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Cotegory)
+admin.site.register(Category)
--- internet_shop/shop/models.py
+++ internet_shop/shop/models.py
@@ -3,8 +3,22 @@ from django.db import models
 
 # Create your models here.
 
-class Cotegory(models.Model):
+def image_product_path(instance, filename):
+    return f"products/{instance.category.name}/{filename}"
+
+
+class Category(models.Model):
     name = models.CharField(max_length=255)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Product(models.Model):
@@ -13,5 +27,18 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
 
-    cotegory = models.ForeignKey(Cotegory,on_delete=models.CASCADE)
+    available = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=image_product_path, blank=True)
 
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} p."
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукция'
