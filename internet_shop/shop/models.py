1dc66c4725b9f0663b53727fdf20d9a3683fcf96
Ne0n2018 <beevertm@gmail.com>
2024-01-30 20:22:06 +0300

    view Product was added. Tamplate Product was created.

--- internet_shop/shop/models.py
+++ internet_shop/shop/models.py
@@ -1,44 +1,71 @@
 from django.db import models
 
 
-# Create your models here.
-
 def image_product_path(instance, filename):
-    return f"products/{instance.category.name}/{filename}"
+    return f"products/{instance.subcategory.name}/{filename}"
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
-    date_created = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    name = models.CharField(verbose_name="Имя категории", max_length=255)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ['name']
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        ordering = ["name"]
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Subcategory(models.Model):
+    name = models.CharField(
+        verbose_name="Имя суб-категории",
+        max_length=255,
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Категория суб-категории",
+    )
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Суб-категория"
+        verbose_name_plural = "Суб-категории"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.FloatField()
-    stock = models.IntegerField()
+    name = models.CharField(verbose_name="Название продукта", max_length=255)
+    description = models.TextField(verbose_name="Описание")
+    price = models.FloatField(verbose_name="Цена")
+    stock = models.IntegerField(verbose_name="Наличие на складе")
 
-    available = models.BooleanField(default=False)
-    image = models.ImageField(upload_to=image_product_path, blank=True)
+    available = models.BooleanField(verbose_name="Доступность", default=False)
+    image = models.ImageField(verbose_name="Изображение", upload_to=image_product_path, blank=True)
 
-    date_created = models.DateField(auto_now_add=True)
-    date_modified = models.DateField(auto_now=True)
+    subcategory = models.ForeignKey(
+        Subcategory,
+        on_delete=models.CASCADE,
+        verbose_name="Суб-категория",
+    )
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.name} - {self.price} p."
+        return f"{self.name} - {self.price} р."
 
     class Meta:
-        ordering = ['name']
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукция'
+        ordering = ["name"]
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
